7ebe07c8741cfe00cea6022925a841320f5f8eb6
AbigailKemuma1 <abigail.kemuma@student.moringaschool.com>
2025-06-08 23:08:45 +0300

    Adds chaiCalculator/chai.js

--- chaiCalculator/chai.js
+++ chaiCalculator/chai.js
@@ -1,1 +1,28 @@
 //Enter your code here//
+function calculateChaiIngredients(numberOfCups) {
+  const waterPerCup = 200; 
+  const milkPerCup = 50;   
+  const teaLeavesPerCup = 1; 
+  const sugarPerCup = 2;     
+
+  const totalWater = waterPerCup * numberOfCups;
+  const totalMilk = milkPerCup * numberOfCups;
+  const totalTeaLeaves = teaLeavesPerCup * numberOfCups;
+  const totalSugar = sugarPerCup * numberOfCups;
+
+  console.log(`To make ${numberOfCups} cups of Kenyan Chai, you will need:`);
+  console.log(`Water: ${totalWater} ml`);
+  console.log(`Milk: ${totalMilk} ml`);
+  console.log(`Tea Leaves (Majani): ${totalTeaLeaves} tablespoons`);
+  console.log(`Sugar (Sukari): ${totalSugar} teaspoons`);
+  console.log(`\nEnjoy your Chai Bora!`);
+}
+
+const input = prompt("Karibu! How many cups of Chai Bora would you like to make?");
+const numberOfCups = Number(input);
+
+if (!isNaN(numberOfCups) && numberOfCups > 0) {
+  calculateChaiIngredients(numberOfCups);
+} else {
+  console.log("Please enter a valid number of cups.");
+}
